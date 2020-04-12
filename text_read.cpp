18ade4da99dddfa87de3600cec3d53cd3e58c1ce
jack850208 <jack850208@gmail.com>
2020-04-13 02:03:18 +0800

    Add files via upload

--- text_read.cpp
+++ text_read.cpp
@@ -3,11 +3,12 @@
 #include "string.h"
 #include "ctype.h"
 
+
 int poschr(char str[],const char* key);
 char *strleng(char *str, int pos, int len);
-void getword(char *s, int n);
+void getword(char *s, int n, int strsize);
 
-char wd[] = "Kind,Lot No,kdkdkd";
+char wd[] = "Kind,Lot No,kdkdkd,good";
 
 
 // Main Program
@@ -17,7 +18,7 @@ int main(int argc, char *argv[]){
 	char str[120];
 	char *ptr,*sp;
 	int k_len;
-	
+
 	// judgement the syntax if correct
 	if(argc != 2){
 		printf("請確認參數是否輸入正確\n");
@@ -36,9 +37,11 @@ int main(int argc, char *argv[]){
 		fgets(str,120,fp);
 		printf("%s",str);
 
+		
+		
 	} while(poschr(str,"R e s u l t   M a p")==-1);
 	
-	getword(wd,2);
+	getword(wd,4,sizeof(wd));
 	
 	fclose(fp);
 	//free(sp);
@@ -79,14 +82,14 @@ char *strleng(char *str, int pos, int len){
 	char *ptr,*sp;
 	int i;
 	
-	sp = (char*)malloc(len*sizeof(char));
+	sp = (char*)malloc((len+1)*sizeof(char));
 	
 	ptr = str + pos * sizeof(char);
 	for(i = 0;i < len;i++){
 		printf("%c",*(ptr+i));
 		sp[i] = *(ptr + i);
-		if(i == (len - 1)) printf("\n");
 	}
+	sp[len] = '\0';
 	
 	//for(i=0;i < len;i++) printf("%c\n",sp[i]);
 	
@@ -94,28 +97,70 @@ char *strleng(char *str, int pos, int len){
 }
 
 
-void getword(char s[], int n){
+void getword(char *s, int n, int strsize){
 	
-	int i = 0, c = 0, temp = 0;
-	
+	int i = 0, wf = 0,k = 0;
+	int temp1 = 0,temp2=0,temp0=0;
+	char *str;
+	printf("%d\n",strsize);
 	do {
-		
-		if(ispunct(s[i])){
-			c++;
-			if(c < n){
-				temp = i;
-			}
-			else{
-				temp = i - temp;
-			}
-		}
-		i++;
-		if(c = n-1){
-			temp = sizeof(s) - i + 1;
-			c++;
-		}
-	} while(c != n);
+		printf("i is:%d\n",i);
+		printf("wf is:%d\n",wf);
+		if(i == 19) k =-1;
+		if(s[i] == ',') {
 	
-	printf("%d\n",i);
-	printf("%d\n",temp);
+
+			wf++;
+		
+			if(n == 1){
+				temp2 = i;
+				temp0 = 0;
+				k = -1;
+				printf("statement1\n");
+			}
+			
+			if((wf == n)&&(n != 1)){
+				temp2 = i;
+				temp0 = temp1+1;
+				k = -1;
+				printf("statement2\n");
+			}
+			
+			/*
+			else{
+				temp2 = strsize;
+				temp0 = temp1;
+				k = -1;
+				printf("statement3\n");
+			}
+			*/
+			temp1 = i;
+		}
+
+		if(k == -1) break;
+		
+		i++;
+
+	} while(1);
+	k = 0;
+	if(wf < n){
+		temp2 = strsize+1;
+		i--;
+		while(k != -1){
+			if(s[i] == ','){
+				temp0 = i + 1;
+				k = -1;
+			}
+			i--;
+		}
+	}
+	
+	printf("temp1:%d,temp2:%d\n",temp0,temp2);
+	
+	str = (char*)malloc((temp2-temp0)*sizeof(char));
+	str = strleng(s,temp0,temp2-temp0);
+	
+	printf("string:%s\n",str);
+
+	free(str);
 }
